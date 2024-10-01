060f66191dd77f98fa6e21c7fa56a7e2cb615a99
AnhTu-Github <101334475+LeAnhTu-Github@users.noreply.github.com>
2024-10-01 21:28:31 +0700

    Complete Vigenere

---
+++ Vigenere/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Vigenere
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtkey = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtInput);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(163, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1190, 174);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(220, 45);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(915, 96);
+            this.txtInput.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(61, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Open";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ban ro";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtOutput);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(163, 290);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1190, 177);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(220, 39);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(915, 96);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(61, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 47);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Open";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ban ma";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtLength);
+            this.groupBox3.Controls.Add(this.txtkey);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(163, 520);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(633, 309);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(338, 101);
+            this.txtLength.Multiline = true;
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(164, 46);
+            this.txtLength.TabIndex = 4;
+            // 
+            // txtkey
+            // 
+            this.txtkey.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtkey.Location = new System.Drawing.Point(61, 188);
+            this.txtkey.Multiline = true;
+            this.txtkey.Name = "txtkey";
+            this.txtkey.Size = new System.Drawing.Size(525, 100);
+            this.txtkey.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Create Random Key";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.randomKey);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Key";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button4);
+            this.groupBox4.Controls.Add(this.button7);
+            this.groupBox4.Controls.Add(this.button6);
+            this.groupBox4.Controls.Add(this.button5);
+            this.groupBox4.Location = new System.Drawing.Point(834, 520);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(507, 309);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Action";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(44, 84);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(186, 55);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Encrypt";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btnEncrypt);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(265, 188);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(186, 55);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "Clear Code";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.btnClearCodetext);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(44, 188);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(186, 55);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "Clear Plantext";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.btnClearPlantext);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(265, 84);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(186, 55);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Decrypt";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1496, 897);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox txtkey;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Vigenere/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vigenere
+{
+    public partial class Form1 : Form
+    {
+        StringBuilder key = new StringBuilder();
+        char[] ASCII= {
+        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
+        'w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',
+        'S','T','U','V','W','X','Y','Z',' ','~','`','!','@','#','$','%','^','&','*','(',')','-',
+        '_','+','=','|','\\','\"','\n','\t','{','[',']','}',':',';','\'','<',',','.','>','?',
+        '/','0','1','2','3','4','5','6','7','8','9'
+    };
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void randomKey(object sender, EventArgs e)
+        {
+            if (txtLength.Text.Length == 0)
+            {
+                MessageBox.Show("Nếu bạn muốn random khóa.Vui lòng cung cấp độ dài khóa.", "Lỗi");
+                return;
+            }
+            int doDai = Convert.ToInt32(txtLength.Text);
+            Random rand = new Random();
+            key.Clear();
+            for (int i = 0; i < doDai; i++)
+            {
+                int randomNumber = rand.Next(ASCII.Length);    
+                char randomChar = ASCII[randomNumber];    
+                key.Append(randomChar);  
+            }
+            txtkey.Text = key.ToString();  
+        }
+
+        private StringBuilder GetKey()
+        {
+            return key;
+        }
+        private void btnEncrypt(object sender, EventArgs e)
+        {
+            // Kiem tra neu khong co khoa
+            if (txtkey.Text.Length == 0)
+            {
+                MessageBox.Show("Khóa không được trống. Vui lòng tạo khóa trước khi mã hóa.", "Lỗi");
+                return;
+            }
+
+            // Kiểm tra xem khóa có hợp lệ không
+            foreach (char c in txtkey.Text)
+            {
+                // Nếu ký tự không nằm trong mảng ASCII, trả về false
+                if (!ASCII.Contains(c))
+                {
+                    MessageBox.Show("Khóa chứa ký tự không hợp lệ. Vui lòng chỉ sử dụng các ký tự hợp lệ.", "Lỗi");
+                    return;
+                }
+            }
+            key.Clear();
+            key.Append(txtkey.Text);
+
+            string vanBanRo = txtInput.Text;
+
+            StringBuilder vanBanMaHoa = new StringBuilder();
+
+
+            for (int i = 0; i < vanBanRo.Length; i++)
+            {
+                char kiTuVanBan = vanBanRo[i];
+                char kiTuKhoa = key[i % key.Length];
+
+
+                int indexVanBan = Array.IndexOf(ASCII, kiTuVanBan);
+                int indexKhoa = Array.IndexOf(ASCII, kiTuKhoa);
+
+                // Nếu ký tự không nằm trong mảng, bỏ qua
+                if (indexVanBan == -1 || indexKhoa == -1)
+                {
+                    vanBanMaHoa.Append(kiTuVanBan);  // Giữ nguyên ký tự không mã hóa
+                }
+                else
+                {
+                    // Mã hóa: tính chỉ số mới trong mảng ASCII
+                    int indexMaHoa = (indexVanBan + indexKhoa) % 97;
+                    vanBanMaHoa.Append(ASCII[indexMaHoa]);  // Thêm ký tự mã hóa vào chuỗi kết quả
+                }
+            }
+
+            txtOutput.Text = vanBanMaHoa.ToString();  // Xuất kết quả mã hóa
+        }
+
+        private void button5_Click(object sender, EventArgs e) { 
+                if (key.Length == 0)
+                {
+                    MessageBox.Show("Khóa không được trống. Vui lòng tạo khóa trước khi giải mã.", "Lỗi");
+                    return; 
+                }
+                // Kiểm tra xem khóa có hợp lệ không
+                foreach (char c in txtkey.Text)
+                {
+                    // Nếu ký tự không nằm trong mảng ASCII, trả về false
+                    if (!ASCII.Contains(c))
+                    {
+                        MessageBox.Show("Khóa chứa ký tự không hợp lệ. Vui lòng chỉ sử dụng các ký tự hợp lệ.", "Lỗi");
+                        return;
+                    }
+                }
+                key.Clear();
+                key.Append(txtkey.Text);
+
+            string vanBanMaHoa = txtOutput.Text; 
+
+                StringBuilder vanBanGiaiMa = new StringBuilder();
+
+     
+                for (int i = 0; i < vanBanMaHoa.Length; i++)
+                {
+                    char kiTuMaHoa = vanBanMaHoa[i];  
+                    char kiTuKhoa = key[i % key.Length];  // Ký tự của khóa tương ứng (lặp lại khóa nếu cần)
+                    int indexMaHoa = Array.IndexOf(ASCII, kiTuMaHoa);
+                    int indexKhoa = Array.IndexOf(ASCII, kiTuKhoa);
+
+
+                    if (indexMaHoa == -1 || indexKhoa == -1)
+                    {
+                        vanBanGiaiMa.Append(kiTuMaHoa); 
+                    }
+                    else
+                    {
+                        // Giải mã: tính chỉ số mới trong mảng ASCII
+                        int indexGiaiMa = (indexMaHoa - indexKhoa + 97) % 97;
+                        vanBanGiaiMa.Append(ASCII[indexGiaiMa]);  // Thêm ký tự đã giải mã vào chuỗi kết quả
+                    }
+                }
+
+                txtInput.Text = vanBanGiaiMa.ToString();  // Xuất kết quả giải mã
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClearPlantext(object sender, EventArgs e)
+        {
+            txtInput.Clear();
+        }
+
+        private void btnClearCodetext(object sender, EventArgs e)
+        {
+            txtOutput.Clear();
+        }
+
+        
+    }
+}
+
